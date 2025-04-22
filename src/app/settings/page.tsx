4fd96dd9a25ea7951bb49af8663a72dd157d055a
kosuke-shiba <kosuke.shiba@gmail.com>
2025-04-22 18:24:04 +0900

    fixed build error

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-//import { useState } from "react";
+import React, { useState } from "react";
 
 export default function SettingsPage() {
     const [loadingLocation, setLoadingLocation] = useState(false);
